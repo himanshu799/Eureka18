f11606d2d3d4377a34a20d446b8e0ad348840d06
Aditiisnghthakur <07aditithakur@gmail.com>
2018-03-29 21:09:23 +0530

    aditi final

--- app/src/main/java/com/example/arpesh/eureka18/SignIn.java
+++ app/src/main/java/com/example/arpesh/eureka18/SignIn.java
@@ -1,25 +1,30 @@
+
 package com.example.arpesh.eureka18;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.math.BigInteger;
+import java.util.regex.Pattern;
 
 /**
  * Created by arpesh on 5/2/18 3:22 AM Eureka18.
  */
 
 public class SignIn extends AppCompatActivity{
-    private EditText InputFname, InputLname,InputMobileno, InputEmail, InputPassword ,InputUserName ;
+    private EditText InputFname, InputLname,InputMobileno, InputEmail, InputPassword ,InputCPassword,InputUserName ;
     private Button SignIn;
     private ProgressBar progressBar;
 
@@ -31,15 +36,29 @@ public class SignIn extends AppCompatActivity{
         setContentView(R.layout.activity_signin);
         progressBar = findViewById(R.id.ProgressBar);
         InputUserName = findViewById(R.id.activity_signin_UserName);
-        InputFname = findViewById(R.id.activity_signin_FirstName);
-        InputLname = findViewById(R.id.activity_signin_LastName);
-        InputMobileno = findViewById(R.id.activity_signin_Mobileno);
+
         InputEmail = findViewById(R.id.activity_signin_Email);
+
+        InputFname = findViewById(R.id.activity_signin_FirstName);
+
+        InputLname = findViewById(R.id.activity_signin_LastName);
+
+        InputMobileno = findViewById(R.id.activity_signin_Mobileno);
+
         InputPassword = findViewById(R.id.activity_signin_Password);
+
+        InputCPassword =findViewById(R.id.activity_signin_CPassword);
+
         SignIn = findViewById(R.id.activity_signinButton);
+
         SignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                try {
+                    register();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
                 try{sendDataToServer();}
                 catch (JSONException e){
                     e.printStackTrace();
@@ -47,7 +66,59 @@ public class SignIn extends AppCompatActivity{
             }
         });
     }
+    public void register() throws  JSONException
+    {
+        FormatDataASJSon();
+        if(!validate())
+        {
+            Toast.makeText(this,"Signup has Failed",Toast.LENGTH_SHORT).show();
+            onDestroy();
+        }
 
+    }
+
+    public boolean validate()
+    {
+        boolean valid= true;
+        if(InputUserName.length()==0||InputUserName.length()>32)
+        {
+            InputUserName.setError("Please Enter Valid Username");
+            valid=false;
+        }
+        if(InputEmail.length()==0)
+        {
+        InputEmail.setError("Please Enter Valid Email Address ");
+            valid=false;
+        }
+        if(InputFname.length()==0)
+        {
+            InputFname.setError("Please Enter The First Name");
+            valid=false;
+        }
+
+        if(InputLname.length()==0)
+        {
+            InputLname.setError("Please Enter The Last Name");
+            valid=false;
+        }
+        if(InputPassword.length()==0)
+        {
+            InputPassword.setError("Please Enter Password");
+            valid=false;
+        }
+        if(InputCPassword.length()==0)
+        {
+            InputCPassword.setError("Please Enter Confirm Password");
+            valid=false;
+        }
+        if(InputPassword.equals(InputCPassword))
+        {
+            Toast.makeText(this,"Password not matching",Toast.LENGTH_SHORT).show();
+            valid = false;
+        }
+
+        return valid;
+    }
     private void sendDataToServer() throws JSONException {
         String type = "register";
 
@@ -59,7 +130,7 @@ public class SignIn extends AppCompatActivity{
     }
 
     private String FormatDataASJSon() throws JSONException {
-       final JSONObject user = new JSONObject();
+        final JSONObject user = new JSONObject();
         String UserName = InputUserName.getText().toString().trim();
         String Fname = InputFname.getText().toString().trim();
         String Lname = InputLname.getText().toString().trim();
